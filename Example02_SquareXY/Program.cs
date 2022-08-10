93b3eebc4248108bb616091ac02ebda7b2e81d44
VaulinDI <vdi85@mail.ru>
2022-08-10 20:45:38 +0300

    squarexy task solved

---
+++ Example02_SquareXY/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число x");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число y");
+int y = Convert.ToInt32(Console.ReadLine());
+if (x == y * y) {
+    Console.WriteLine("Число x является квадратом числа y");
+}
+else if (y == x * x) {
+    Console.WriteLine("Число y является квадратом числа x");
+}
+else {
+    Console.WriteLine("Ни число x не является квадратом числа y, ни число y не является квадратом числа x");
+}
