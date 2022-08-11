1238222bb5e4908de218963054b4b187cf84adb0
VaulinDI <vdi85@mail.ru>
2022-08-11 19:44:38 +0300

    sumdigit task solved

---
+++ Example06_SumDigits/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+int sum = 0;
+while (i <= a) {
+    sum = sum + i;
+    i++;
+}
+Console.WriteLine(sum);
