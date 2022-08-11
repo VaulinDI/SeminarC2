b9bd8ba8c8d4f64c52a7051a0739ab75c5e59759
VaulinDI <vdi85@mail.ru>
2022-08-11 20:01:40 +0300

    countdigits task solved

---
+++ Example07_CountDigits/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+double x = 2;
+int d = 10;
+int count = 0;
+while (x > 1) {
+    x = num / d;
+    d = d * 10;
+    count++;
+}
+Console.WriteLine("Количество цифр в числе: " + count);
