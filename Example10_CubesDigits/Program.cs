057d8ecb51c2935e5d534247c51c362e559f0417
VaulinDI <vdi85@mail.ru>
2022-08-12 20:02:18 +0300

    cubesdigits task solved

---
+++ Example10_CubesDigits/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите количество чисел n, для которых будут считаться кубы от 1 до n : ");
+int n = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+int cube = 1;
+while (i <= n) {
+    cube = i * i * i;
+    if (cube % 2 == 0) {
+        Console.WriteLine("Тот самый куб: " + cube);
+    }
+    i++;
+}
