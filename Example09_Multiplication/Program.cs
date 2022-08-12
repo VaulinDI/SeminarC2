a686d1f7de62fbb828dbf6e12bef78db579138ff
VaulinDI <vdi85@mail.ru>
2022-08-12 19:48:57 +0300

    multiplication task solved

---
+++ Example09_Multiplication/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+int mul = 1;
+while (i <= n) {
+    mul = mul * i;
+    i++;
+}
+Console.WriteLine("Произведение чисел от 1 до n равно: " + mul);
